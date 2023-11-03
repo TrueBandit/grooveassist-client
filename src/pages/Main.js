a9adfe01825276f18a735818123942b537f69430
boaz <boaz@avnery.com>
2023-11-03 14:27:33 +0000

    Landing page update

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,12 +6,14 @@ import Generator from './Generator'
 import Landing from './Landing'
 import Login from './Login'
 import SignUp from './SignUp';
+import { Height } from '@mui/icons-material';
 
 function Main() {
 
   return (
     <div style={{textAlign:'center'}}>
     <Navbar/>
+    <div style={{height: '55px'}}></div>
     <Routes>
       <Route path="community" element={<Community/>} />
       <Route path="generate" element={<Generator/>} />
